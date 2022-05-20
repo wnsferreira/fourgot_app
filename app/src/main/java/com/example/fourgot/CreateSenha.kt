f7e541f03a769d43e1e82f59cee67dadcd98d2bd
gabir-dm <dmelo.gabriela@gmail.com>
2022-05-20 08:35:00 -0300

    Adjusted icons, replaced some hardcoded strings and minor changes to layout and filenames

--- app/src/main/java/com/example/fourgot/CreateSenha.kt
+++ app/src/main/java/com/example/fourgot/CreateSenha.kt
@@ -3,9 +3,9 @@ package com.example.fourgot
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity3 : AppCompatActivity() {
+class CreateSenha : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main3)
+        setContentView(R.layout.create_senha)
     }
 }
