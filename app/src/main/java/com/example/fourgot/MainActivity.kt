ba8669ba90bb824a5c1c85f77ff67a6a69271325
thais.schontag@al.infnet.edu.br <thais.schontag@al.infnet.edu.br>
2022-05-20 19:09:38 -0300

    Added screen transition

--- app/src/main/java/com/example/fourgot/MainActivity.kt
+++ app/src/main/java/com/example/fourgot/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.fourgot
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnCard1 = this.findViewById<ImageButton>(R.id.btnCard1)
+
+        btnCard1.setOnClickListener {
+            val intent = Intent(this, ReadSenha::class.java)
+
+            startActivity(intent)
+        }
+
+        val addSenha = this.findViewById<ImageButton>(R.id.addSenha)
+
+        addSenha.setOnClickListener {
+            val intent = Intent(this, CreateSenha::class.java)
+
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/fourgot/ReadSenha.kt
+++ app/src/main/java/com/example/fourgot/ReadSenha.kt
@@ -1,11 +1,30 @@
 package com.example.fourgot
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class ReadSenha : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.read_senha)
+
+        val btnVoltar = this.findViewById<ImageButton>(R.id.btnVoltar)
+
+        btnVoltar.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+
+            startActivity(intent)
+        }
+
+        val btnEditar = this.findViewById<ImageButton>(R.id.btnEditar)
+
+        btnEditar.setOnClickListener {
+            val intent = Intent(this, CreateSenha::class.java)
+
+            startActivity(intent)
+        }
+
     }
 }
