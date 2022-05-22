69fa10f83f7b28da1bab75164b70fed0c1be0f15
Alceu-wv <alceu.wv@gmail.com>
2022-05-22 10:41:15 -0300

    process

--- app/src/main/java/com/example/fourgot/ui/password/PasswordViewModel.kt
+++ app/src/main/java/com/example/fourgot/ui/password/PasswordViewModel.kt
@@ -1,49 +1,49 @@
-package com.example.fourgot.ui.password
-
-import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.example.fourgot.R
-import com.example.fourgot.repository.VaultRepository
-import kotlinx.coroutines.launch
-import java.lang.Exception
-
-class PasswordViewModel(
-    private val repository: VaultRepository
-) : ViewModel() {
-
-    // notifica quando uma senha foi cadastrada
-    private val _passwordStateEventData = MutableLiveData<PasswordState>()
-    val passwordStateEventData: LiveData<PasswordState>
-        get() = _passwordStateEventData
-
-    private val _messageEventData = MutableLiveData<Int>()
-    val messageEventData: LiveData<Int>
-        get() = _messageEventData
-
-    fun addPassword(name: String, password: String) = viewModelScope.launch {
-        try {
-            val id = repository.insertVault(name, password)
-            if (id > 0){
-                _passwordStateEventData.value = PasswordState.Inserted
-                _messageEventData.value = R.string.password_inserted_successfully
-            }
-        }catch (ex: Exception){
-            _messageEventData.value = R.string.password_error_to_insert
-            Log.e(TAG,ex.toString())
-        }
-    }
-
-    companion object {
-        private val TAG = PasswordViewModel::class.java.simpleName
-    }
-
-    sealed class PasswordState {
-        object Inserted : PasswordState()
-    }
-
-}
-
-
+//package com.example.fourgot.ui.password
+//
+//import android.util.Log
+//import androidx.lifecycle.LiveData
+//import androidx.lifecycle.MutableLiveData
+//import androidx.lifecycle.ViewModel
+//import androidx.lifecycle.viewModelScope
+//import com.example.fourgot.R
+//import com.example.fourgot.repository.VaultRepository
+//import kotlinx.coroutines.launch
+//import java.lang.Exception
+//
+//class PasswordViewModel(
+//    private val repository: VaultRepository
+//) : ViewModel() {
+//
+//    // notifica quando uma senha foi cadastrada
+//    private val _passwordStateEventData = MutableLiveData<PasswordState>()
+//    val passwordStateEventData: LiveData<PasswordState>
+//        get() = _passwordStateEventData
+//
+//    private val _messageEventData = MutableLiveData<Int>()
+//    val messageEventData: LiveData<Int>
+//        get() = _messageEventData
+//
+//    fun addPassword(name: String, password: String) = viewModelScope.launch {
+//        try {
+//            val id = repository.insertVault(name, password)
+//            if (id > 0){
+//                _passwordStateEventData.value = PasswordState.Inserted
+//                _messageEventData.value = R.string.password_inserted_successfully
+//            }
+//        }catch (ex: Exception){
+//            _messageEventData.value = R.string.password_error_to_insert
+//            Log.e(TAG,ex.toString())
+//        }
+//    }
+//
+//    companion object {
+//        private val TAG = PasswordViewModel::class.java.simpleName
+//    }
+//
+//    sealed class PasswordState {
+//        object Inserted : PasswordState()
+//    }
+//
+//}
+//
+//
--- app/src/main/java/com/example/fourgot/ui/passwordList/PasswordListFragment.kt
+++ app/src/main/java/com/example/fourgot/ui/passwordList/PasswordListFragment.kt
@@ -1,65 +1,65 @@
-package com.example.fourgot.ui.passwordList
-
-import androidx.lifecycle.ViewModelProvider
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModel
-import androidx.navigation.fragment.findNavController
-import com.example.fourgot.R
-import com.example.fourgot.data.db.AppDatabase
-import com.example.fourgot.data.db.dao.VaultDAO
-import com.example.fourgot.data.db.entity.VaultEntity
-import com.example.fourgot.repository.DatabaseDataSource
-import com.example.fourgot.repository.VaultRepository
-import com.example.fourgot.ui.password.PasswordViewModel
-import kotlinx.android.synthetic.main.fragment_password_list.*
-
-class PasswordListFragment : Fragment(R.layout.fragment_password_list) {
-
-
-    private val viewModel: PasswordListViewModel by viewModels {
-        object :ViewModelProvider.Factory {
-            override fun <T : ViewModel> create(modelClass: Class<T>): T {
-                val vaultDAO: VaultDAO =
-                    AppDatabase.getInstance(requireContext()).vaultDAO
-
-                val repository: VaultRepository = DatabaseDataSource(vaultDAO)
-                return  PasswordListViewModel(repository) as T
-            }
-        }
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        observeViewModelEvents()
-        configureViewListeners()
-    }
-
-    private fun observeViewModelEvents(){
-        viewModel.allPasswordsEvent.observe(viewLifecycleOwner) {   allPasswords ->
-            val passwordListAdapter = PasswordListAdapter(allPasswords)
-
-            with(recycler_password){
-                setHasFixedSize(true)
-                adapter = passwordListAdapter
-            }
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        viewModel.getPasswords()
-    }
-
-    // transicao de tela
-    private fun configureViewListeners(){
-        fabAddPassword.setOnClickListener{
-            findNavController().navigate(R.id.passwordFragment)
-        }
-    }
-
-}
+//package com.example.fourgot.ui.passwordList
+//
+//import androidx.lifecycle.ViewModelProvider
+//import android.os.Bundle
+//import androidx.fragment.app.Fragment
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import androidx.fragment.app.viewModels
+//import androidx.lifecycle.ViewModel
+//import androidx.navigation.fragment.findNavController
+//import com.example.fourgot.R
+//import com.example.fourgot.data.db.AppDatabase
+//import com.example.fourgot.data.db.dao.VaultDAO
+//import com.example.fourgot.data.db.entity.VaultEntity
+//import com.example.fourgot.repository.DatabaseDataSource
+//import com.example.fourgot.repository.VaultRepository
+//import com.example.fourgot.ui.password.PasswordViewModel
+//import kotlinx.android.synthetic.main.fragment_password_list.*
+//
+//class PasswordListFragment : Fragment(R.layout.fragment_password_list) {
+//
+//
+//    private val viewModel: PasswordListViewModel by viewModels {
+//        object :ViewModelProvider.Factory {
+//            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+//                val vaultDAO: VaultDAO =
+//                    AppDatabase.getInstance(requireContext()).vaultDAO
+//
+//                val repository: VaultRepository = DatabaseDataSource(vaultDAO)
+//                return  PasswordListViewModel(repository) as T
+//            }
+//        }
+//    }
+//
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//        observeViewModelEvents()
+//        configureViewListeners()
+//    }
+//
+//    private fun observeViewModelEvents(){
+//        viewModel.allPasswordsEvent.observe(viewLifecycleOwner) {   allPasswords ->
+//            val passwordListAdapter = PasswordListAdapter(allPasswords)
+//
+//            with(recycler_password){
+//                setHasFixedSize(true)
+//                adapter = passwordListAdapter
+//            }
+//        }
+//    }
+//
+//    override fun onResume() {
+//        super.onResume()
+//        viewModel.getPasswords()
+//    }
+//
+//    // transicao de tela
+//    private fun configureViewListeners(){
+//        fabAddPassword.setOnClickListener{
+//            findNavController().navigate(R.id.passwordFragment)
+//        }
+//    }
+//
+//}
